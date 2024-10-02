9dba3e945c28728078cd4548c20207592e9eabcd
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-10-02 18:11:20 +0100

    até Seção 4, aula 2

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    defaultCommandTimeout: 5000,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
