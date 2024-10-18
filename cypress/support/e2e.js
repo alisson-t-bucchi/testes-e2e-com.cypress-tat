05745586830d1ea00987fa28b0a347a2bb35ef54
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-10-18 15:08:59 +0100

    Configure cypress grep

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,6 +17,12 @@
 import './commands'
 import 'cypress-mailosaur'
 import 'cypress-iframe'
+import registerCypressGrep from '@cypress/grep/src/support'
+registerCypressGrep()
+
+// Outros imports aqui.
+
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
